12cb8a3c9ac94a342e06f0241e7f217ff5a30782
RayFFH <57190209+RayFFH@users.noreply.github.com>
2024-01-16 18:58:01 +0000

    deploying

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,34 @@ import shoushinAudio from './assets/shoushin.mp3';
 import icchisuruAudio from './assets/icchisuru.mp3';
 import tatoebaAudio from './assets/tatoeba.mp3';
 import seikiAudio from './assets/seiki.mp3';
-import './input.css';  // Import your styles if needed
+import './input.css'; // Import your styles if needed
+
+
+const TitlePage = ({ onStartQuiz }) => (
+  <div className="min-h-screen bg-slate-800 text-white text-center flex flex-col items-center justify-center">
+    <h1 className="text-6xl font-extrabold uppercase mb-8 rounded-full">WELCOME TO JAPANESE PITCH ACCENT TRAINER!</h1>
+    <p className="text-2xl mb-4 rounded-full">Japanese pitch accent is the fluxation of low and high pitch sounds within a word</p>
+    <p className="text-2xl mb-4 rounded-full">Listen carefuly and type the correct Pitch Fluctation using H and L</p>
+    <p className="text-2xl mb-4 rounded-full">Are you ready to test your Pitch Accent knowledge?</p>
+    <button
+      className="mt-8 px-6 py-3 border border-white rounded-full text-xl font-bold hover:bg-white hover:text-slate-800 transition duration-300"
+      onClick={onStartQuiz}
+    >
+      Start Quiz
+    </button>
+    <br></br>
+    <img
+      src="/public/pitch.jpg"  // Replace with the actual path to your image
+      alt="Hiragana Quiz Logo"     // Provide an appropriate alt text
+      className="mb-8 rounded-full centered"
+      style={{ width: '500px', height: '300px', margin: 'auto' }}  // Adjust the width and height as needed
+    />
+  </div>
+);
 
 function App() {
   const audioRef = useRef(null);
+  const [showTitle, setShowTitle] = useState(true);
 
   const hiragana = [
     { accent: 'LH', hiragana: 'ちょうめ', meaning: 'district of a town', audio: choumeAudio },
@@ -17,31 +41,106 @@ function App() {
     { accent: 'HL', hiragana: 'せいき', meaning: 'for example', audio: seikiAudio },
   ];
 
+  const handleStartQuiz = () => {
+    setShowTitle(false);
+    setRandomHiragana();
+  };
+
   const [input, setInput] = useState('');
   const [current, setCurrent] = useState(0);
+  const [streak, setStreak] = useState(0);
+  const [maxStreak, setMaxStreak] = useState(0);
+  const [error, setError] = useState(false);
+
+  const [playbackSpeed, setPlaybackSpeed] = useState(1);
 
   const setRandomHiragana = () => {
     const randomIndex = Math.floor(Math.random() * hiragana.length);
     setCurrent(randomIndex);
+  
+    // Update audio source
+    const newAudio = new Audio(hiragana[randomIndex].audio);
+    newAudio.playbackRate = playbackSpeed;
+    newAudio.addEventListener('loadeddata', () => {
+      // Once the audio has loaded, update the audioRef
+      audioRef.current = newAudio;
+      playAudio();
+    });
+  };
 
-    // Play the audio
-    audioRef.current.src = hiragana[randomIndex].audio;
-    audioRef.current.play();
+  const handleSpeedChange = (speed) => {
+    setPlaybackSpeed(speed);
+    audioRef.current.playbackRate = speed;
+    playAudio(); // Play the audio again when the speed changes
+  };
+
+  const playAudio = () => {
+    audioRef.current.play().catch(error => {
+      console.error('Audio playback failed:', error);
+    });
   };
 
   const handleChange = (e) => {
     setInput(e.target.value);
   };
 
-  useEffect(() => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    console.log('Input:', input.toLowerCase());
+    console.log('Accent:', hiragana[current].accent.toLowerCase());
+  
+    if (input.toLowerCase() === hiragana[current].accent.toLowerCase()) {
+      console.log('Correct answer!');
+      setStreak(streak + 1);
+      setMaxStreak(streak + 1 > maxStreak ? streak + 1 : maxStreak);
+      setError(false);
+  
+      localStorage.setItem('streak', streak + 1);
+      localStorage.setItem('maxStreak', streak + 1 > maxStreak ? streak + 1 : maxStreak);
+    } else {
+      console.log('Wrong answer!');
+      console.log('Current Streak:', streak);
+      const h = hiragana[current].hiragana;
+      const a = hiragana[current].accent;
+      console.log('Correct Accent:', a.toLowerCase());
+      setError(`Wrong! The correct answer for ${h} is ${a}`);
+      console.log('Error State:', error); // Log the error state
+      setStreak(0);
+      console.log('Reset Streak:', 0);
+      localStorage.setItem('streak', 0);
+    }
+  
+    setInput('');
     setRandomHiragana();
-  }, []);
+    //playAudio();
+  };
+  
+  useEffect(() => {
+    // Log the error state after the component has re-rendered
+    setRandomHiragana();
+    setStreak(parseInt(localStorage.getItem('streak')) || 0);
+    setMaxStreak(parseInt(localStorage.getItem('maxStreak')) || 0);
+    console.log('Error State:', error);
+  }, [error]);
+
+  // useEffect(() => {
+  //   setRandomHiragana();
+  //   setStreak(parseInt(localStorage.getItem('streak')) || 0);
+  //   setMaxStreak(parseInt(localStorage.getItem('maxStreak')) || 0);
+  // }, []);
 
   return (
-    <div className="min-h-screen bg-slate-800 text-white text-center">
-      {/* Audio element */}
-      <audio ref={audioRef} />
-
+    <>
+      {showTitle ? (
+        <TitlePage onStartQuiz={handleStartQuiz} />
+      ) : (
+        <div className="min-h-screen bg-slate-800 text-white text-center">
+          {/* Audio element */}
+          <audio ref={audioRef}>
+            <source src={hiragana[current].audio} type="audio/mpeg" />
+          </audio>
+  
       <header className="p-6 mb-8">
         <h1 className="text-2xl font-bold uppercase">Hiragana Quiz</h1>
         <div>
@@ -49,11 +148,11 @@ function App() {
           <p>Streak: {streak} / Max Streak: {maxStreak}</p>
         </div>
       </header>
-
+  
       <div className="text-9xl font-bold mb-8">
         <p>{hiragana[current].hiragana}</p>
       </div>
-
+  
       <div className="mb-8">
         <form onSubmit={handleSubmit}>
           <input
@@ -63,16 +162,48 @@ function App() {
             className="block w-24 bg-transparent border-b-2 border-b-white mx-auto outline-none text-center text-6xl pb-2"
           />
         </form>
+          {/* Display error message */}
+    {error && (
+      <div className="text-red-500 mb-4">
+        <p>{error}</p>
       </div>
-
-      {/* Display error message if any */}
-      {error && 
-        <div>
-          <p>{ error }</p>
-        </div>
-      }
-    </div>
-  );
-}
+    )}
+      </div>
+   {/* Play Audio button */}
+   <div className="mt-4">
+        <button onClick={() => handleSpeedChange(1)} className="px-4 py-2 border border-white">
+          Play Audio
+        </button>
+      </div>
+{/* Playback speed controls */}
+<div className="mt-4 flex justify-center space-x-4">
+  <button
+    onClick={() => handleSpeedChange(0.5)}
+    className="px-4 py-2 border border-white mr-3"
+  >
+    0.5x
+  </button>
+  <br></br>
+  <button
+    onClick={() => handleSpeedChange(1)}
+    className="px-4 py-2 border border-white mr-10"
+  >
+    1x
+  </button>
+  <br></br>
+  <button
+    onClick={() => handleSpeedChange(1.5)}
+    className="px-4 py-2 border border-white"
+  >
+    1.5x
+  </button>
+</div>
+</div>
+)}
+</>
+);
+    }
+    
+  
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
+const colors = require('tailwindcss/colors') 
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+module.exports = { 
+    content: ["./src/**/*.{html,js,jsx}"], 
+    theme: { 
+        extend: { 
+            colors: { 
+                transparent: 'transparent', 
+                current: 'currentColor', 
+                black: colors.black, 
+                white: colors.white, 
+                emerald: colors.emerald, 
+                indigo: colors.indigo, 
+                yellow: colors.yellow, 
+                stone: colors.warmGray, 
+                sky: colors.lightBlue, 
+                neutral: colors.trueGray, 
+                gray: colors.coolGray, 
+                slate: colors.blueGray, 
+                lime: colors.lime, 
+                rose: colors.rose, 
+            }, 
+        }, 
+    }, 
+    plugins: [], 
+  }
